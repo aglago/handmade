4055796d5664a4989d0c6aebe829cb77f7b6f738
amimanye <amimanyeaglago@gmail.com>
2024-02-18 22:49:32 +0000

    changed the width of productincategorycontainer

--- src/components/1SearchPage/ListOfProducts/ListOfProducts.jsx
+++ src/components/1SearchPage/ListOfProducts/ListOfProducts.jsx
@@ -15,37 +15,37 @@ const allProducts = [
                                 id: 0,
                                 name: "Women suits",
                                 price: "GHc 200",
-                                image: ""
+                                image: "https://kikiclothing.com/cdn/shop/products/IMG-9235_730x1024.jpg?v=1621542767"
                             },
                             {
                                 id: 0,
                                 name: "Women suits",
                                 price: "GHc 200",
-                                image: ""
+                                image: "https://i.pinimg.com/564x/f8/a4/35/f8a435cfcd7238531cd23157df7a9929.jpg"
                             },
                             {
                                 id: 0,
                                 name: "Women suits",
                                 price: "GHc 200",
-                                image: ""
+                                image: "https://i.pinimg.com/originals/b1/b7/ea/b1b7ea184dd036c7fcb2bef24b21b363.png"
                             },
                             {
                                 id: 0,
                                 name: "Women suits",
                                 price: "GHc 200",
-                                image: ""
+                                image: "https://www.bellanaijaweddings.com/wp-content/uploads/2019/04/Desire-Collection-Sima-Brew-2019-BellaNaija-Weddings-12.jpg"
                             },
                             {
                                 id: 0,
                                 name: "Women suits",
                                 price: "GHc 200",
-                                image: ""
+                                image: "https://lh5.googleusercontent.com/4XYm1k2qe6-NaQm3dvIyh6alfDoFIuFcftL1ArH1iksP0QK6hCEyieXtcMTyLUAO8xSzuVNq4-Ji3x2RxvBUjG-NOkznWs5b6ksjLr0FSPSZMHZ8JvqdgDB7iLxl8LnnzY_tlioqc8FRvVaPLg"
                             },
                             {
                                 id: 0,
                                 name: "Women suits",
                                 price: "GHc 200",
-                                image: ""
+                                image: "https://i.pinimg.com/736x/f8/77/b7/f877b7a68fc8ede3e4976e0abc4cd800.jpg"
                             },
                         ]
                     },
