c163bb0462eb13ef4ff87086a46a39997b0121f5
amimanye <amimanyeaglago@gmail.com>
2024-02-19 22:49:19 +0000

    fixed the search container problem

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,18 @@ import ShopPage from "./components/0ShopPage/0Shop/ShopPage"
 import SearchPage from "./components/1SearchPage/1Search/SearchPage"
 
 export default function App() {
+
+  // if it is the shop / home then
+  //   return ShopPage
+  // else if it is the SearchPage,
+  //   return SearchPage
   
   return <div>
     {/* The Shop Page of the App */}
     {/* <ShopPage /> */}
 
     <SearchPage />
+
+
   </div>
 }
